140cc06e24ed809263e16a9a5e70cad618e741d2
Pratik Kulkarni <pratikkulkarni1307@gmail.com>
2023-06-17 17:28:17 +0530

    Add  next authentication using google.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { Inter } from "next/font/google"
 import { cn } from "../lib/utils"
 import Navbar from "@/components/Navbar"
+import { Toaster } from '@/components/Toaster';
 
 export const metadata = {
   title: 'Breadit',
@@ -23,6 +24,7 @@ export default function RootLayout({
         <div className='container max-w-7xl mx-auto h-full pt-12'>
           {children}
         </div>
+        <Toaster />
       </body>
     </html>
   )
--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,14 +1,37 @@
 "use client"
+
 import React, { FC, useState } from 'react'
 import { Button } from './ui/Button'
 import { cn } from '@/lib/utils'
 import { Icons } from './Icons';
+import { signIn } from "next-auth/react"
+import { useToast } from '../hooks/use-toast';
+import { Toast } from './ui/Toast';
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
     const [isLoading, setIsLoading] = useState(false);
-    const loginWithGoogle = () => { }
+    const toast = useToast();
+    const loginWithGoogle = async () => {
+
+        setIsLoading(true)
+
+        try {
+            // throw new Error("something happened man!!")
+            await signIn('google')
+            // throw ("")
+        } catch (error) {
+            Toast({
+                title: 'Error happened man!',
+                variant: 'destructive'
+            })
+
+        } finally {
+            setIsLoading(false);
+        }
+
+    }
 
     return (
         <div className={cn('flex justify-center', className)} {...props}>
