e89cd52a8917478f585eb023e6a408f7ac3bb876
Pratik Kulkarni <pratikkulkarni1307@gmail.com>
2023-06-20 11:20:09 +0530

    Add subreddit properly.

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -6,28 +6,61 @@ import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 import { useMutation } from "@tanstack/react-query"
 import { CreateSubredditPayload } from '@/lib/validators/subreddit';
+import { useCustomToasts } from '@/hooks/use-custom-toast';
+import { toast } from "@/hooks/use-toast"
+import { error } from 'console';
 
 function page() {
 
   const [input, inputSet] = useState('');
   const router = useRouter();
+  const { loginToast } = useCustomToasts();
+
   const { mutate: createCommunity, isLoading } = useMutation({
     mutationFn: async () => {
 
       const payload: CreateSubredditPayload = {
         name: input
       }
-
       const response = await fetch("/api/subreddit", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
         body: JSON.stringify(payload),
-      });
-      console.log(response);
-
-    }
+      })
+      if (!response.ok) {
+        if (response.status === 401) {
+          return loginToast()
+        }
+        if (response.status === 422) {
+          return toast({
+            title: 'Invalid subreddit name.',
+            description: 'Please choose a name between 3 and 21 letters.',
+            variant: 'destructive',
+          })
+        }
+        if (response.status === 409) {
+          return toast({
+            title: 'Subreddit already exists.',
+            description: 'Please choose a different name.',
+            variant: 'destructive',
+          })
+        }
+        toast({
+          title: "There was an error.",
+          description: "Could not create a subreddit.",
+          variant: "destructive",
+        })
+      } else {
+        router.push(`/r/${input}`);
+        toast({
+          title: "Success",
+          description: "Subreddit created successfully.",
+          variant: "default",
+        })
+      }
+    },
   })
 
   return (
@@ -59,13 +92,13 @@ function page() {
 
         <div className='flex justify-end gap-4'>
           <Button
-            // disabled={isLoading}
+            disabled={isLoading}
             variant='subtle'
             onClick={() => router.back()}>
             Cancel
           </Button>
           <Button
-            // isLoading={isLoading}
+            isLoading={isLoading}
             disabled={input.length === 0}
             onClick={() => createCommunity()}>
             Create Community
